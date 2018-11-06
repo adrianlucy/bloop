029fa4cd12c4cf375caebadfefd57f9a1746251a
Adrian Lucy <lucy@Adrians-MacBook-Pro.local>
2018-11-06 14:34:14 -0600

    Hello

---
+++ code.py
@@ -0,0 +1,5 @@
+
+def do_something():
+    x='Hi'
+    y='iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii!'
+    print(x+y)# this will make it much easier in future problems to see that something is actually happening
