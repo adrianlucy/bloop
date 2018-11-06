1f4eea8fa083112eb7a86cb65a2024512ea53649
Christian <c_rodriguez@umail.ucsb.edu>
2018-11-06 13:51:01 -0800

    Added some comments

--- code.py
+++ code.py
@@ -2,4 +2,5 @@
 def do_something():
     x='Hi'
     y='iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii!'
+    z = 'world'
     print(x+y)# this will make it much easier in future problems to see that something is actually happening
